eec78a490c7a235fb1f51feb4710ac9b13b5c309
Maxim Petrov <max.zh.petrov@gmail.com>
2022-08-24 17:47:45 +0300

    Update app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Header from '../header';
 import TaskList from '../task-list';
@@ -6,141 +6,129 @@ import Footer from '../footer';
 
 import './app.css';
 
-class App extends Component {
-  maxId = 100;
-
-  state = {
-    tasks: [],
-    statuses: {
-      completed: 'completed',
-      editing: 'editing',
-      uncompleted: 'uncompleted',
-    },
-    filter: 'all',
+const App = () => {
+  const [filter, setFilter] = useState('all');
+  const [tasks, setTasks] = useState([]);
+  const statuses = {
+    completed: 'completed',
+    editing: 'editing',
+    uncompleted: 'uncompleted',
   };
 
-  deleteItem = (id) => {
-    this.setState(({ tasks }) => ({ tasks: tasks.filter(({ id: el }) => el !== id) }));
+  const deleteItem = (id) => {
+    setTasks(() => tasks.filter(({ id: el }) => el !== id));
   };
 
-  addItem = (value, minutes, seconds) => {
-    const status = this.state.statuses.uncompleted;
+  const addItem = (value, minutes, seconds) => {
+    const { uncompleted } = statuses;
     const item = {
-      id: this.maxId++,
-      status: status,
-      prevStatus: status,
+      id: maxId++,
+      status: uncompleted,
+      prevStatus: uncompleted,
       text: value,
       createTime: new Date().getTime(),
       checked: false,
       stopwatchTime: minutes * 60 * 1000 + seconds * 1000,
       playerState: false,
     };
-    this.setState(({ tasks }) => ({ tasks: [...tasks, item] }));
+    setTasks(() => [...tasks, item]);
   };
 
-  deleteCompleted = () => {
-    this.setState(({ tasks, statuses: { completed } }) => {
-      return { tasks: tasks.filter(({ status }) => status !== completed) };
-    });
+  const deleteCompleted = () => {
+    const { completed } = statuses;
+    setTasks(() => tasks.filter(({ status }) => status !== completed));
   };
 
-  changeFilter = (filter) => {
-    this.setState({ filter: filter });
+  const changeFilter = (filter) => {
+    setFilter(filter);
   };
 
-  filterItems(items, filter, filters) {
+  const filterItems = (items, filter, filters) => {
     const { completed, uncompleted } = filters;
     if (filter === 'all') return items;
     if (filter === 'uncompleted') return items.filter(({ status }) => status === uncompleted);
     if (filter === 'completed') return items.filter(({ status }) => status === completed);
-  }
-
-  doneToggle = (id) => {
-    this.setState(({ tasks, statuses: { completed, uncompleted } }) => {
-      return {
-        tasks: tasks.map((el) => {
-          let { status, checked } = el;
-          if (id === el.id) {
-            el.status = status === completed ? uncompleted : completed;
-            el.checked = !checked;
-          }
-          return el;
-        }),
-      };
-    });
   };
 
-  editingBtn = (id) => {
-    this.setState(({ tasks, statuses: { editing } }) => {
-      return {
-        tasks: tasks.map((el) => {
-          if (id === el.id) {
-            el.prevStatus = el.status;
-            el.status = editing;
-          }
-          return el;
-        }),
-      };
-    });
-  };
-
-  editingText = (id, text) => {
-    this.setState(({ tasks }) => {
-      return {
-        tasks: tasks.map((el) => {
-          if (id === el.id) {
-            el.text = text;
-            el.status = el.prevStatus;
-          }
-          return el;
-        }),
-      };
-    });
-  };
-
-  updateStopwatchTime = (id, time, playerState) => {
-    this.setState(({ tasks }) => {
-      return {
-        tasks: tasks.map((el) => {
-          if (id === el.id) {
-            el.stopwatchTime = time;
-            el.playerState = playerState;
-          }
-          return el;
-        }),
-      };
-    });
-  };
-
-  render() {
-    const { tasks, filter, statuses } = this.state;
-    const { completed } = statuses;
-    const sizeUncompleted = tasks.reduce((acc, { status }) => (acc += status !== completed), 0);
-    const visibly = this.filterItems(tasks, filter, statuses);
-
-    return (
-      <section className="todoapp">
-        <Header addItem={(text, minutes, seconds) => this.addItem(text, minutes, seconds)} />
-        <section className="main">
-          <TaskList
-            tasks={visibly}
-            statuses={statuses}
-            onDeleted={(id) => this.deleteItem(id)}
-            doneToggle={(id, checked) => this.doneToggle(id, checked)}
-            editingBtn={(id) => this.editingBtn(id)}
-            editingText={(id, text) => this.editingText(id, text)}
-            updateStopwatchTime={(id, time, playerState) => this.updateStopwatchTime(id, time, playerState)}
-          />
-          <Footer
-            sizeUncompleted={sizeUncompleted}
-            filter={filter}
-            deleteCompleted={() => this.deleteCompleted()}
-            changeFilter={(text) => this.changeFilter(text)}
-          />
-        </section>
-      </section>
+  const doneToggle = (id) => {
+    const { completed, uncompleted } = statuses;
+    setTasks(() =>
+      tasks.map((el) => {
+        let { status, checked } = el;
+        if (id === el.id) {
+          el.status = status === completed ? uncompleted : completed;
+          el.checked = !checked;
+        }
+        return el;
+      })
     );
-  }
-}
+  };
+
+  const editingBtn = (id) => {
+    const { editing } = statuses;
+    setTasks(() =>
+      tasks.map((el) => {
+        if (id === el.id) {
+          el.prevStatus = el.status;
+          el.status = editing;
+        }
+        return el;
+      })
+    );
+  };
+
+  const editingText = (id, text) => {
+    setTasks(() =>
+      tasks.map((el) => {
+        if (id === el.id) {
+          el.text = text;
+          el.status = el.prevStatus;
+        }
+        return el;
+      })
+    );
+  };
+
+  const updateStopwatchTime = (id, time, playerState) => {
+    setTasks(() =>
+      tasks.map((el) => {
+        if (id === el.id) {
+          el.stopwatchTime = time;
+          el.playerState = playerState;
+        }
+        return el;
+      })
+    );
+  };
+
+  const { completed } = statuses;
+  const sizeUncompleted = tasks.reduce((acc, { status }) => (acc += status !== completed), 0);
+  const visibly = filterItems(tasks, filter, statuses) || [];
+
+  return (
+    <section className="todoapp">
+      <Header addItem={(text, minutes, seconds) => addItem(text, minutes, seconds)} />
+      <section className="main">
+        <TaskList
+          tasks={visibly}
+          statuses={statuses}
+          onDeleted={(id) => deleteItem(id)}
+          doneToggle={(id, checked) => doneToggle(id, checked)}
+          editingBtn={(id) => editingBtn(id)}
+          editingText={(id, text) => editingText(id, text)}
+          updateStopwatchTime={(id, time, playerState) => updateStopwatchTime(id, time, playerState)}
+        />
+        <Footer
+          sizeUncompleted={sizeUncompleted}
+          filter={filter}
+          deleteCompleted={() => deleteCompleted()}
+          changeFilter={(text) => changeFilter(text)}
+        />
+      </section>
+    </section>
+  );
+};
+
+let maxId = 100;
 
 export default App;
